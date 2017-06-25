5e59eec37b0700bf599cf169cf6750f92b7e2843
funa1g <funa1g@gmail.com>
2017-06-25 10:34:36 +0900

    Implement counting some parameters

--- analyze.py
+++ analyze.py
@@ -1,11 +1,20 @@
-import sys
+import argparse
 from lib import TextAnalyzer
 
+
 def main(file_path: str):
+    """execute TextAnalyzer
+    """
     analyzer = TextAnalyzer()
     analyzer.read(file_path)
     analyzer.execute()
 
 if __name__ == "__main__":
     # execute only if run as a script
-    main(sys.argv[1])
+    parser = argparse.ArgumentParser(description='Analyzing text')
+    parser.add_argument('text_file_path', metavar='f', type=str,
+                        help='analyzing text file path')
+    parser.add_argument('-T', dest='text', metavar='T', type=str,
+                        required=False, help='analyzing text')
+    args = parser.parse_args()
+    main(args.text_file_path)
--- lib/analyzer.py
+++ lib/analyzer.py
@@ -3,14 +3,31 @@ class TextAnalyzer(object):
     """
     def __init__(self):
         self.path = ""
+        self.lines = []
+        self.paragraphs = []
+        self.word_count = 0
 
     def read(self, file_path: str):
         """
         file_path: str
         """
         self.path = file_path
+        f = open(self.path, 'r')
+        for paragraph in f:
+            self.paragraphs.append(paragraph.strip())
+            lines = paragraph.split('。')
+            for line in lines:
+                line = line.strip()
+                if not line:
+                    continue
+                self.lines.append(line)
+        for paragraph in self.paragraphs:
+            self.word_count += len(paragraph)
 
     def execute(self):
         """output analyzing result
         """
         print('path: ' + self.path)
+        print('文字数: ' + str(self.word_count))
+        print('行数: ' + str(len(self.lines)))
+        print('段落数: ' + str(len(self.paragraphs)))
