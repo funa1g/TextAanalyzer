a08646a829e8ac7eb55e4fb0161e127ea017749b
funa1g <funa1g@gmail.com>
2017-06-26 22:41:55 +0900

    Aggregate paragraph information to object

--- lib/analyzer.py
+++ lib/analyzer.py
@@ -1,11 +1,32 @@
+from typing import Sequence
+
+
+class Paragraph(object):
+    def __init__(self, paragraph):
+        self.text = paragraph.strip()
+        self.lines = []
+        self.parse()
+
+    def parse(self):
+        lines = self.split_text(self.text)
+        for line in lines:
+            line = line.strip()
+            if not line:
+                continue
+            self.lines.append(line)
+
+    def split_text(self, text: str) -> Sequence[str]:
+        return text.split('。')
+
+
 class TextAnalyzer(object):
     """module implementation
     """
     def __init__(self):
         self.path = ""
-        self.lines = []
         self.paragraphs = []
         self.word_count = 0
+        self.line_count = 0
 
     def read(self, file_path: str):
         """
@@ -13,21 +34,20 @@ class TextAnalyzer(object):
         """
         self.path = file_path
         f = open(self.path, 'r')
-        for paragraph in f:
-            self.paragraphs.append(paragraph.strip())
-            lines = paragraph.split('。')
-            for line in lines:
-                line = line.strip()
-                if not line:
-                    continue
-                self.lines.append(line)
+        for text in f:
+            text = text.strip()
+            if not text:
+                continue
+            paragraph = Paragraph(text)
+            self.paragraphs.append(paragraph)
         for paragraph in self.paragraphs:
-            self.word_count += len(paragraph)
+            self.word_count += len(paragraph.text)
+            self.line_count += len(paragraph.lines)
 
     def execute(self):
         """output analyzing result
         """
         print('path: ' + self.path)
         print('文字数: ' + str(self.word_count))
-        print('行数: ' + str(len(self.lines)))
+        print('行数: ' + str(self.line_count))
         print('段落数: ' + str(len(self.paragraphs)))
